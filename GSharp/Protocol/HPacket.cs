18dcec14865406463402a285eb4194fabec1e010
Nicholas <64442818+iUseYahoo@users.noreply.github.com>
2023-06-05 13:36:35 +1000

    Update HPacket.cs

--- GSharp/Protocol/HPacket.cs
+++ GSharp/Protocol/HPacket.cs
@@ -451,9 +451,8 @@ public class HPacket
 
     public void FixLength()
     {
-        var remember = _isEdited;
-        Replace(0, BytesLength - 4);
-        _isEdited = remember;
+        var length = BytesLength - 4;
+        length.ToBytes().CopyTo(_bytes, 0);
     }
     
     public override bool Equals(object? obj)
